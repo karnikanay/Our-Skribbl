f6f288e24d660424dad439fd4377d6e18c7014ec
Anay <karnikanay@gmail.com>
2020-05-19 02:13:45 +0530

    Initialize canvas.js

---
+++ src/client/canvas.js
@@ -0,0 +1,33 @@
+// Get DOM elements
+const canvas = document.getElementById('board');
+const context = canvas.getContext('2d');
+
+var brushRadius;
+var curPos, lastPos;
+var drawColor;
+var curTool;
+
+export function setTool(newTool) {
+
+}
+
+export function setBrushSize(newBrushSize) {
+
+}
+
+export function applyTool() {
+
+}
+
+export function setColor(newColor) {
+
+}
+
+function fillColor() {
+
+}
+
+function brushStroke() {
+
+}
+

a2f68a853e7c1a21e617cce64bd01051c5a9ab74
Anay <karnikanay@gmail.com>
2020-05-19 16:49:33 +0530

    Autogenerate color palette

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,16 @@
-document.write('This is Skribbl');
+import { COLOR_PALETTE } from '../shared/constants'
+
+var colorPalette = document.getElementById('colorPalette');
+
+function initColors() {
+  for(var id = 0; id < COLOR_PALETTE.length; id++) {
+    var newColor = document.createElement("div");
+    newColor.setAttribute("class", "color");
+    newColor.setAttribute("id", "color" + id);
+    newColor.style.backgroundColor = COLOR_PALETTE[id];
+    colorPalette.appendChild(newColor);
+  }
+}
+
+initColors();
+

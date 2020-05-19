aa561374108d2f1dd48b57ea4eecef2cd75d2d88
Anay <karnikanay@gmail.com>
2020-05-19 21:00:20 +0530

    Add colors to palette

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,8 @@ import { COLOR_PALETTE } from '../shared/constants'
 var colorPalette = document.getElementById('colorPalette');
 
 function initColors() {
+  colorPalette.style.width = (38*(COLOR_PALETTE.length/2 + COLOR_PALETTE.length%2) + 10) + "px";
+
   for(var id = 0; id < COLOR_PALETTE.length; id++) {
     var newColor = document.createElement("div");
     newColor.setAttribute("class", "color");
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,12 +1,26 @@
 module.exports = Object.freeze({
  
-  COLOR_PALETTE: ["#000000", // Black
-                  "#FFFFFF", // White
-                  "#FF0000", // Red
-                  "#00FF00", // Green
-                  "#0000FF", // Blue
-                  "#FF00FF", // Magenta
-                  "#FFFF00", // Yellow
-                  "#00FFFF"] // Cyan
-
+  COLOR_PALETTE: ["#FFF",
+                  "#C1C1C1", 
+                  "#EF130B", 
+                  "#FF7100", 
+                  "#FFE400", 
+                  "#00CC00", 
+                  "#00B2FF", 
+                  "#231FD3", 
+                  "#A300BA", 
+                  "#D37CAA", 
+                  "#A0522D",
+                  "#000",
+                  "#4C4C4C", 
+                  "#740B07", 
+                  "#C23800", 
+                  "#E8A200", 
+                  "#005510", 
+                  "#00569E", 
+                  "#0E0865", 
+                  "#550069", 
+                  "#A75574", 
+                  "#63300D"]
+ 
 });

9ed648023da6c183eaca4f0b3bf26f5399d7a516
Anay <karnikanay@gmail.com>
2020-05-19 15:47:10 +0530

    Add COLOR_PALETTE to constants

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,3 +1,12 @@
 module.exports = Object.freeze({
-  
+ 
+  COLOR_PALETTE = ["#000000", // Black
+                   "#FFFFFF", // White
+                   "#FF0000", // Red
+                   "#00FF00", // Green
+                   "#0000FF", // Blue
+                   "#FF00FF", // Magenta
+                   "#FFFF00", // Yellow
+                   "#00FFFF"] // Cyan
+
 });

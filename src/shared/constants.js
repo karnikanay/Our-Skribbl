c129f326edf9bf0fc897b648f4df04728cbbac17
Anay <karnikanay@gmail.com>
2021-04-14 13:56:39 +0530

    Reformat constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,39 +1,43 @@
 module.exports = Object.freeze({
-
-  MSG_TYPES: { CONNECT: "connect",
-               BRUSH_STROKE: "brush_stroke",
-               CLEAR_CANVAS: "clear_canvas",
-               FILL_COLOR: "fill_color",
-               DISCONNECT: "disconnect" },
+  MSG_TYPES: { 
+    CONNECT: "connect",
+    BRUSH_STROKE: "brush_stroke",
+    CLEAR_CANVAS: "clear_canvas",
+    FILL_COLOR: "fill_color",
+    DISCONNECT: "disconnect" 
+  },
  
-  BRUSH_SIZES: [ 2,
-                 5,
-                 10,
-                 15,
-                 20,
-                 30 ],
+  BRUSH_SIZES: [ 
+    2,
+    5,
+    10,
+    15,
+    20,
+    30 
+  ],
 
-  COLOR_PALETTE: ['#FFFFFF',
-                  '#C1C1C1', 
-                  '#EF130B', 
-                  '#FF7100', 
-                  '#FFE400', 
-                  '#00CC00', 
-                  '#00B2FF', 
-                  '#231FD3', 
-                  '#A300BA', 
-                  '#D37CAA', 
-                  '#A0522D',
-                  '#000000',
-                  '#4C4C4C', 
-                  '#740B07', 
-                  '#C23800', 
-                  '#E8A200', 
-                  '#005510', 
-                  '#00569E', 
-                  '#0E0865', 
-                  '#550069', 
-                  '#A75574', 
-                  '#63300D']
- 
+  COLOR_PALETTE: [
+    '#FFFFFF',
+    '#C1C1C1', 
+    '#EF130B', 
+    '#FF7100', 
+    '#FFE400', 
+    '#00CC00', 
+    '#00B2FF', 
+    '#231FD3', 
+    '#A300BA', 
+    '#D37CAA', 
+    '#A0522D',
+    '#000000',
+    '#4C4C4C', 
+    '#740B07', 
+    '#C23800', 
+    '#E8A200', 
+    '#005510', 
+    '#00569E', 
+    '#0E0865', 
+    '#550069', 
+    '#A75574', 
+    '#63300D'
+  ]
 });

712d1cc95ebb7d277b41cf85e7529667cd42c646
Anay <karnikanay@gmail.com>
2021-04-16 01:22:36 +0530

    Setup ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ const path = require('path');
 module.exports = {
   mode: "development",
   entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js",
